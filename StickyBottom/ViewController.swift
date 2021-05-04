967ecf798cc6e8fe3510c03137776d519cbe721e
Sittithorn Ch <sittithorn.c-external@tcs.com>
2021-05-04 10:57:00 +0700

    Auto Stick to bottom

--- StickyBottom/ViewController.swift
+++ StickyBottom/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollview: UIScrollView!
+    @IBOutlet weak var buttonTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var myButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+   }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let contenHeight = scrollview.contentSize.height
+        if contenHeight > self.view.bounds.height, contenHeight != 0 {
+            buttonTopConstraint.constant = 20
+        } else if contenHeight != 0 {
+            let topConstraint = self.view.bounds.height - scrollview.contentSize.height - myButton.bounds.height
+            buttonTopConstraint.constant = topConstraint
+        }
+    }
 
 }
 
